b8e28582c0d6ab593424e1ad8b2d0c75674438c2
cr-zhichen <296529530@qq.com>
2023-03-29 22:10:39 +0800

    加入基础页面框架

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
 
 import {ref, computed} from 'vue'
 
-import Home from './components/HomePage.vue'
-import NotFound from './components/NotFound.vue'
+import Home from './page/HomePage.vue'
+import NotFound from './page/NotFoundPage.vue'
+import LoginPage from './page/LoginPage.vue'
+import ArticlePage from './page/ArticlePage.vue'
+import ManagePage from "./page/ManagePage.vue";
 
 
 //======路由=====//
 const routes = {
   '/': Home,
+  '/login': LoginPage,
+  '/article': ArticlePage,
+  '/manage': ManagePage,
 }
 const currentPath = ref(window.location.hash)
 window.addEventListener('hashchange', () => {
@@ -23,6 +29,9 @@ const currentView = computed(() => {
 <template>
 
   <a href="#/">Home</a> |
+  <a href="#/login">Login</a> |
+  <a href="#/article">Article</a> |
+  <a href="#/manage">Manage</a> |
   <a href="#/non-existent-path">Broken Link</a>
   <component :is="currentView"/>
 
---
+++ src/page/ManagePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({})
+</script>
+
+<template>
+  <h1>ManagePage</h1>
+</template>
+
+<style scoped>
+
+</style>
