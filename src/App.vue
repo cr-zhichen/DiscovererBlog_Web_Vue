e30116aec96baf8151a744fc9ac369650efb8a97
cr-zhichen <296529530@qq.com>
2023-03-29 22:02:32 +0800

    完成简单路由的创建

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {ref, computed} from 'vue'
+
+import Home from './components/HomePage.vue'
+import NotFound from './components/NotFound.vue'
+
+
+//======路由=====//
+const routes = {
+  '/': Home,
+}
+const currentPath = ref(window.location.hash)
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <a href="#/">Home</a> |
+  <a href="#/non-existent-path">Broken Link</a>
+  <component :is="currentView"/>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({})
+</script>
+
+<template>
+  <h1>NotFound</h1>
+</template>
+
+<style scoped>
+
+</style>
