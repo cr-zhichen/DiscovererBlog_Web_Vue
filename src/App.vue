76ef10802253984e7e7eea890fd31ed5b514e9ea
cr-zhichen <296529530@qq.com>
2023-03-30 09:22:15 +0800

    加入读取public 文件夹内文件的方法

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 
-import {ref, computed} from 'vue'
+import {computed, ref} from 'vue'
+import {getConfig, getForPath} from './tool/readPublic'
 
 import Home from './page/HomePage.vue'
 import NotFound from './page/NotFoundPage.vue'
@@ -24,15 +25,20 @@ const currentView = computed(() => {
   return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
 
+//======变量=====//
+
+
+//======方法=====//
+
 </script>
 
 <template>
 
-  <a href="#/">Home</a> |
-  <a href="#/login">Login</a> |
-  <a href="#/article">Article</a> |
-  <a href="#/manage">Manage</a> |
-  <a href="#/non-existent-path">Broken Link</a>
+  <a href="#/">首页</a> |
+  <a href="#/login">登录</a> |
+  <a href="#/article">文章</a> |
+  <a href="#/manage">管理</a> |
+  <a href="#/non-existent-path">404</a>
   <component :is="currentView"/>
 
 </template>
