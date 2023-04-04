3219e4c984875ad5be7d1d2870b7b08343269919
cr-zhichen <296529530@qq.com>
2023-04-04 10:58:20 +0800

    修改为可服务器访问url

--- src/tool/PostAPI.js
+++ src/tool/PostAPI.js
@@ -1,4 +1,5 @@
-const url = "http://127.0.0.1:8080";
+// const url = "http://127.0.0.1:8080";
+const url = "https://discoverer-blog-back.ccrui.cn";
 
 const loginUrl = url + "/User/Login";//登录
 const registerUrl = url + "/User/Register";//注册
