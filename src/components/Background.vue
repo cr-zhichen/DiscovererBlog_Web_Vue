1aaac9c30e4bdd92b2d6479e1dfa94673199e37a
zgccrui <zgccrui@outlook.com>
2023-05-02 17:35:19 +0800

    修复背景图片在带鱼屏上的显示问题

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,35 +1,18 @@
+<script setup>
+
+import {ref} from "vue";
+
+const imageUrl = ref('https://img-cdn.ccrui.cn/images/2020/07/18/20200330121127_FfvVm.jpg')
+
+</script>
+
+
 <template>
     <div class="background"
          :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
-
     </div>
 </template>
 
-<script>
-export default {
-    name: "Background",
-    data() {
-        return {
-            imageUrl: 'https://img-cdn.ccrui.cn/images/2020/07/18/20200330121127_FfvVm.jpg',
-            imageAlt: 'background'
-        }
-    },
-    methods: {
-        setBackgroundSize() {
-            const element = this.$el;
-            const width = element.offsetWidth;
-            const height = element.offsetHeight;
-            const aspectRatio = width / height;
-            const imageAspectRatio = 1920 / 1080; // 假设图片宽高比为 16:9
-            if (aspectRatio > imageAspectRatio) {
-                element.style.backgroundSize = 'cover';
-            } else {
-                element.style.backgroundSize = 'contain';
-            }
-        }
-    }
-}
-</script>
 
 <style scoped>
 .background {
@@ -38,7 +21,7 @@ export default {
     left: 0;
     height: 100%; /* 设置背景组件的高度为视口高度 */
     width: 100%; /* 设置背景组件的宽度为100% */
-    /*background-size: cover; !* 等比缩放背景图像以填充整个组件 *!*/
+    background-size: cover; /* 等比缩放背景图像以填充整个组件 */
     background-position: center; /* 将背景图像置于组件中心 */
     background-repeat: no-repeat; /* 让背景不重复 */
     opacity: 1.0; /* 设置透明度为50% */
@@ -51,7 +34,6 @@ export default {
 }
 
 .dark .background {
-
     opacity: 0.5;
 }
 
